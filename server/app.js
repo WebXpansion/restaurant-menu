730ee5b363fcca26257bc61e2844bfdae7a82cc9
WebXpansion <contact@webxpansion.com>
2026-02-24 23:24:50 +0100

    push

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-
+import cookieParser from "cookie-parser";
 
 
 import internalRoutes from "./routes/internal.routes.js";
