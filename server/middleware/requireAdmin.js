1fb1a8d0ce2c494c64f2aa554de56d760096aba8
WebXpansion <contact@webxpansion.com>
2026-02-25 02:04:55 +0100

    push

--- server/middleware/requireAdmin.js
+++ server/middleware/requireAdmin.js
@@ -1,22 +1,13 @@
 export function requireAdmin(req, res, next) {
-  console.log("SESSION USER (requireAdmin):", req.session.user); 
+  if (!req.session?.user) {
+    return res.redirect("/admin/login");
+  }
 
-  console.log("REQ RESTAURANT:", req.restaurant.id);
-  console.log("TYPES:",
-    typeof req.session.user?.restaurant_id,
-    typeof req.restaurant.id
-  );
-
-  console.log("SESSION USER dans requireAdmin:", req.session.user);
-
-
-  if (!req.session.user) {
-    return res.redirect(`/admin/login`);
+  if (!req.restaurant) {
+    return res.status(400).send("Restaurant context missing");
   }
 
   if (req.session.user.restaurant_id !== req.restaurant.id) {
-    
-    console.log("MISMATCH DETECTED");
     return res.status(403).send("Forbidden");
   }
 
