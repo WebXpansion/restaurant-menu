762a495f048422e18af9bcd5de86176979fe0f91
WebXpansion <contact@webxpansion.com>
2026-02-25 14:04:36 +0100

    loading

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,19 +1,24 @@
 const loader = document.getElementById("siteLoader");
-      
-if (!sessionStorage.getItem("loaderShown")) {
 
-  window.addEventListener("load", () => {
+window.addEventListener("load", () => {
+
+  if (!sessionStorage.getItem("loaderShown")) {
+
     setTimeout(() => {
       loader.classList.add("hide");
 
       setTimeout(() => {
         loader.style.display = "none";
+        document.body.classList.remove("loading");
       }, 800);
 
       sessionStorage.setItem("loaderShown", "true");
-    }, 2000);
-  });
 
-} else {
-  loader.style.display = "none";
-}
+    }, 2000);
+
+  } else {
+    loader.style.display = "none";
+    document.body.classList.remove("loading");
+  }
+
+});
