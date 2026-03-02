5461880db7dedc887088e6cbdd10c6aac47e20e1
WebXpansion <contact@webxpansion.com>
2026-03-02 15:37:49 +0100

    je commence le front de la page landing

--- public/js/three-demo.js
+++ public/js/three-demo.js
@@ -1,7 +1,9 @@
-import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "https://unpkg.com/three@0.160.0/examples/jsm/loaders/DRACOLoader.js";
+import * as THREE from "three";
+import { OrbitControls } from "/vendor/three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "/vendor/three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "/vendor/three/examples/jsm/loaders/DRACOLoader.js";
+
+
 
 const canvases = document.querySelectorAll(".card-3d canvas");
 
